b6e96b803795bd1d44b3cfd5aef213ab54c444f5
Erin Goncer and Nancy Fang <nancy@>
2016-04-07 17:32:07 -0700

    Added in logic for determining winner

--- js/script.js
+++ js/script.js
@@ -1,37 +1,9 @@
-//Business Logic
-
-// var turns = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-//
-//
-// // player one is the first click and all odd clicks.
-// function Player (turns) {
-//   for (var = i; i <= turns.length; i++) {
-//     if (turns[i] % 2 === 0) {
-//
-//     }
-//   }
-// }
-//
-// function Game ()
-// // all odd clicks are player one. first click is player one. player one displays x's.
-// //all even clicks are player two and return O's.
-//
-// function Plot ()
-// // has three states. empty on load. if clicked on an odd count then dispays x. if clicked on even count then displays o's. filled spaces are not clickable and do not increase the counter
-//
-// function Board ()
-// // The game is won if all spaces in a row are filled and the same OR all spaces in a column are filled and the same OR a1 = b2 = c3 OR c1 = b2 = a3.
-
-//UI Logic
-
-// function Game (turns) {
-//
-// }
-
-function Plot (coordinate, value) {
+//BUSINESS LOGIC
+function Plot (coordinate, valueNum) {
   this.coordinate = coordinate;
-  this.value = value;
+  this.valueNum = valueNum;
 }
+
 
 var a1 = new Plot ("a1", 0);
 var b1 = new Plot ("b1", 0);
@@ -45,63 +17,57 @@ var a3 = new Plot ("a3", 0);
 var b3 = new Plot ("b3", 0);
 var c3 = new Plot ("c3", 0);
 
+
 var board = [a1, b1, c1, a2, b2, c2, a3, b3, c3];
-// function winCheck() {
-//   if (((a1 !== 0) && (a1 === b1) && (b1 === c1)) || ((a2 !== 0) && (a2 === b2) && (b2 === c2)) || ((a3 !== 0) && (a3 === b3) && (b3 === c3))) {
-//   alert("Win!");
-// }
+
+
 function winCheck() {
-  if (a1.value !== 0) {
-    alert("Win!");
-  } else {
-    alert("No winner yet");
+  if ((a1.valueNum !== 0) && (a1.valueNum === b1.valueNum) && (b1.valueNum === c1.valueNum) || (a2.valueNum !== 0) && (a2.valueNum === b2.valueNum) && (b2.valueNum === c2.valueNum) || (a3.valueNum !== 0) && (a3.valueNum === b3.valueNum) && (b3.valueNum === c3.valueNum) || (a1.valueNum !== 0) && (a1.valueNum === a2.valueNum) && (a2.valueNum === a3.valueNum) || (b1.valueNum !== 0) && (b1.valueNum === b2.valueNum) && (b2.valueNum === b3.valueNum) ||
+  (c1.valueNum !== 0) && (c1.valueNum === c2.valueNum) && (c2.valueNum === c3.valueNum) ||
+  (a1.valueNum !== 0) && (a1.valueNum === b2.valueNum) && (b2.valueNum === c3.valueNum) ||
+  (a3.valueNum !== 0) && (a3.valueNum === b2.valueNum) && (b2.valueNum === c1.valueNum)) {
+  alert("win!");
   }
-};
+  else {
+    console.log("not yet");
+  }
 
-// function winCheck() {
-//   if (((a1.value !== 0) && (a1.value === b1.value) && (b1.value === c1.value)) || ((a2.value !== 0) && (a2.value === b2.value) && (b2.value === c2.value)) || ((a3.value !== 0) &&  (a3.value === b3.value) && (b3.value === c3.value))) {
-//     alert("Win!");
-//   } else {
-//     alert("No winner yet");
-//   }
-// };
+}
+var turn = 0;
 
+//UI LOGIC
 $(document).ready(function() {
   $("h1").hide().fadeIn(1900);
 
-  var turn = 0;
-
   $(".plot").click(function() {
     turn++;
-    console.log(turn);
+    winCheck();
     if (turn % 2 === 0) {
       $(this).addClass("o");
+      var id = $(this).attr('id');
+      for (i = 0; i <= board.length; i++) {
+        if (board[i].coordinate === id) {
+          return board[i].valueNum += 1;
+        }
+        else {
+          console.log("2");
+        }
+      }
+    }
+    else {
+      $(this).addClass("x");
 
       var id = $(this).attr('id');
       for (i = 0; i <= board.length; i++) {
         if (board[i].coordinate === id) {
-          return board[i].value += 1;
-          console.log(board[i].coordinate);
-      }
-    }
-  }
-       else {
-        $(this).addClass("x");
-
-        var id = $(this).attr('id');
-        for (i = 0; i <= board.length; i++) {
-          if (board[i].coordinate === id) {
-            return board[i].value += 2;
-            alert(board[i]);
-          }
+          return board[i].valueNum += 2;
+        }
+        else {
+          console.log("3");
         }
       }
-      winCheck();
+    }
+
   });
+
 });
-
-
-// The game is won if all spaces in a row are filled and the same OR all spaces in a column are filled and the same OR a1 = b2 = c3 OR c1 = b2 = a3.
-//
-
-//
